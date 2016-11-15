822af091ebce09346570d1ef167ef0a6f6fd1bf6
Oyvind Habberstad <oyvindhabberstad@gmail.com>
2016-11-15 23:14:19 +0100

    MVC Form + Bootstrap

---
+++ src/main/java/com/driw/configuration/ThymeleafConfiguration.java
@@ -0,0 +1,30 @@
+package com.driw.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
+
+@Configuration
+public class ThymeleafConfiguration {
+    @Autowired
+    private ThymeleafProperties properties;
+
+    @Value("${spring.thymeleaf.templates_root:}")
+    private String templatesRoot;
+
+    @Bean
+    public ITemplateResolver defaultTemplateResolver() {
+        TemplateResolver resolver = new FileTemplateResolver();
+        resolver.setSuffix(properties.getSuffix());
+        resolver.setPrefix(templatesRoot);
+        resolver.setTemplateMode(properties.getMode());
+        resolver.setCharacterEncoding(properties.getEncoding().toString());
+        resolver.setCacheable(properties.isCache());
+        return resolver;
+    }
+}
--- src/main/java/com/driw/web/product/ProductController.java
+++ src/main/java/com/driw/web/product/ProductController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Controller
 @RequestMapping(value = "/")
@@ -23,9 +25,9 @@ class ProductController {
         this.productService = productService;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    ModelAndView getPriceListForAllProducts(Model model){
-        ModelAndView modelAndView = new ModelAndView("index");
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    ModelAndView getPriceList(){
+        ModelAndView modelAndView = new ModelAndView("list");
         List<Product> productList = productService.findAll();
         List<ProductViewModel> productModelList = new ArrayList<>();
         for (Product product : productList ) {
@@ -34,5 +36,76 @@ class ProductController {
             }
         }
         return modelAndView.addObject("products", productModelList);
+    }
+
+    @RequestMapping(value = "/calculator", method = RequestMethod.GET)
+    ModelAndView getForm(InputForm inputForm){
+        ModelAndView modelAndView = new ModelAndView("calculator");
+        List<Product> productList = productService.findAll();
+        Set<Option> optionList = new HashSet<>();
+        for(Product product : productList) {
+            optionList.add(new Option(product.getId(), product.getName()));
+        }
+        return modelAndView.addObject("options", optionList);
+    }
+
+    @RequestMapping(value = "/calculator", method = RequestMethod.POST)
+    String postForm(InputForm inputForm){
+        return "calculator";
+    }
+
+    public static final class InputForm {
+        private long productId;
+        private int numberOfPackages;
+        private int numberOfUnits;
+
+        public long getProductId() {
+            return productId;
+        }
+        public void setProductId(long productId) {
+            this.productId = productId;
+        }
+
+        public int getNumberOfPackages() {
+            return numberOfPackages;
+        }
+
+        public void setNumberOfPackages(int numberOfPackages) {
+            this.numberOfPackages = numberOfPackages;
+        }
+
+        public int getNumberOfUnits() {
+            return numberOfUnits;
+        }
+
+        public void setNumberOfUnits(int numberOfUnits) {
+            this.numberOfUnits = numberOfUnits;
+        }
+    }
+
+    public static final class Option {
+        private long value;
+        private String text;
+
+        public Option(long value, String text){
+            this.value = value;
+            this.text = text;
+        }
+
+        public long getValue() {
+            return value;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public void setValue(long value) {
+            this.value = value;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
     }
 }
